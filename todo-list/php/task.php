eee6fd47e821d03bbead8039f10122e6e9d53839
janetchepkurui942 <131593412+janetchepkurui942@users.noreply.github.com>
2023-06-08 01:39:56 +0300

    Delete task.php

--- todo-list/php/task.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Check if user is logged in
-session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-  // User not logged in, redirect to login page
-  header('Location: index.html');
-  exit();
-}
-?>
